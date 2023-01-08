7c603f73e166781d720a1a3b1939ca8b547c2440
dave89gray <102819156+dave89gray@users.noreply.github.com>
2023-01-08 15:07:11 +0000

    Further playing about with Python

--- run.py
+++ run.py
@@ -4,24 +4,26 @@
 
 class Employee:
     
+    num_of_emps = 0
+    raise_amount = 1.04
+
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.pay = pay
         self.email = first + '.' + last + '@company.com'
 
+        Employee.num_of_emps += 1
+
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
+print(Employee.num_of_emps)
 
 emp_1 = Employee('David', 'Gray', 50000)
 emp_2 = Employee('Test', 'User', 60000)
 
-# print(emp_1)
-# print(emp_2)
-
-#print(emp_1.email)
-#print(emp_2.email)
-
-print(emp_1.fullname())
-print(Employee.fullname(emp_1))
-print(emp_2.fullname())
+print(Employee.num_of_emps)
