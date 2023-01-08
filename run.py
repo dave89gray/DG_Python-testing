13c1d30fb6b2c0f6ad38053b9518aa3b61acbddb
dave89gray <102819156+dave89gray@users.noreply.github.com>
2023-01-08 14:33:47 +0000

    Testing Python to try and get back into it

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+class Employee:
+    
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+emp_1 = Employee('David', 'Gray', 50000)
+emp_2 = Employee('Test', 'User', 60000)
+
+# print(emp_1)
+# print(emp_2)
+
+print(emp_1.email)
+print(emp_2.email)
