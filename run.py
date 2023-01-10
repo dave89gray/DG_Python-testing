37f5e6865d5237acb920127f3f08bcc23737755c
dave89gray <102819156+dave89gray@users.noreply.github.com>
2023-01-10 19:24:25 +0000

    More Python Playing

--- run.py
+++ run.py
@@ -4,16 +4,13 @@
 
 class Employee:
     
-    num_of_emps = 0
     raise_amount = 1.04
 
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
+        self.email = first + '.' + last + '@email.com'
         self.pay = pay
-        self.email = first + '.' + last + '@company.com'
-
-        Employee.num_of_emps += 1
 
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
@@ -21,33 +18,37 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
 
-    @classmethod
-    def set_raise_amt(cls, amount):
-        cls.raise_amount = amount
+class Developer(Employee):
+    raise_amount = 1.10
 
-    @classmethod
-    def from_string(cls, emp_str):
-        first, last, pay = emp_str.split('-')
-        return cls(first, last, pay)
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
 
-    @staticmethod
-    def is_workday(day):
-        if day.weekday() == 5 or day.weekday() == 6:
-            return False
-        return True
+class Manager(Employee):
 
-emp_1 = Employee('David', 'Gray', 50000)
-emp_2 = Employee('Test', 'User', 60000)
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
 
-import datetime
-my_date = datetime.date(2016, 7, 11)
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+    
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
 
-print(Employee.is_workday(my_date))
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.fullname())
 
-#emp_str_1 = 'John-Doe-70000'
-#emp_str_2 = 'Steve-Smith-30000'
-#emp_str_3 = 'Jane-Doe-90000'
+dev_1 = Developer('David', 'Gray', 50000, 'Python')
+dev_2 = Developer('Test', 'User', 60000, 'Java')
 
-#new_emp_1 = Employee.from_string(emp_str_1)
-#print(new_emp_1.email)
-#print(new_emp_1.pay)
+mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
+
+print(issubclass(Manager, Developer))
