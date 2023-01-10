5dcfc7da868e88b26491dcba2f4b0699ed6e89a4
dave89gray <102819156+dave89gray@users.noreply.github.com>
2023-01-10 12:16:50 +0000

    Additional playing around with python and classes

--- run.py
+++ run.py
@@ -21,9 +21,33 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
 
-print(Employee.num_of_emps)
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amount = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
 
 emp_1 = Employee('David', 'Gray', 50000)
 emp_2 = Employee('Test', 'User', 60000)
 
-print(Employee.num_of_emps)
+import datetime
+my_date = datetime.date(2016, 7, 11)
+
+print(Employee.is_workday(my_date))
+
+#emp_str_1 = 'John-Doe-70000'
+#emp_str_2 = 'Steve-Smith-30000'
+#emp_str_3 = 'Jane-Doe-90000'
+
+#new_emp_1 = Employee.from_string(emp_str_1)
+#print(new_emp_1.email)
+#print(new_emp_1.pay)
