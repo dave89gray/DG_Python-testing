424f6cf2b406bf52b907a277fee65ae62324d18b
dave89gray <102819156+dave89gray@users.noreply.github.com>
2023-01-11 18:37:56 +0000

    Working with Decorators

--- run.py
+++ run.py
@@ -3,46 +3,37 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 class Employee:
-    
-    raise_amount = 1.04
 
-    def __init__(self, first, last, pay):
+    def __init__(self, first, last):
         self.first = first
         self.last = last
-        self.email = first + '.' + last + '@email.com'
-        self.pay = pay
 
+    @property
+    def email(self):
+        return '{}.{}@email.com'.format(self.first, self.last)
+
+    @property
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
-    def apply_raise(self):
-        self.pay = int(self.pay * self.raise_amount)
+    @fullname.setter
+    def fullname(self, name):
+        first, last = name.split(' ')
+        self.first = first
+        self.last = last
 
-    def __repr__(self):
-        return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
+    @fullname.deleter
+    def fullname(self):
+        print('Delete Name!')
+        self.first = None
+        self.last = None
 
-    def __str__(self):
-        return '{} - {}'.format(self.fullname(), self.email)
-        
-    def __add__(self, other):
-        return self.pay + other.pay
+emp_1 = Employee('David', 'Gray')
 
-    def __len__(self):
-        return len(self.fullname())
+emp_1.fullname = 'Davie Gray'
 
-emp_1 = Employee('David', 'Gray', 50000)
-emp_2 = Employee('Test', 'User', 60000)
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.fullname)
 
-print(len(emp_1))
-
-print('test'.__len__())
-
-#print(emp_1 + emp_2)
-
-#print(emp_1)
-
-#print(repr(emp_1))
-#print(str(emp_1))
-##print(emp_1.__repr__())
-#print(emp_1.__str__())
-
+del emp_1.fullname
