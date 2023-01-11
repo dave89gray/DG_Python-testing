f684eb2f77610f6abf86196cf0f72e43190f8f34
dave89gray <102819156+dave89gray@users.noreply.github.com>
2023-01-11 19:28:21 +0000

    Playing with more decorators

--- run2.py
+++ run2.py
@@ -1,13 +1,38 @@
 # Decorators
+from functools import wraps
 
-def outer_function(msg):
+def my_logger(orig_func):
+    import logging
+    logging.basicConfig(filename='{}.log'.format(orig_func.__name__), level=logging.INFO)
 
-    def inner_function():
-        print(msg)
-    return inner_function
+    @wraps(orig_func)
+    def wrapper(*args, **kwargs):
+        logging.info(
+            'Ran with args: {}, and kwargs: {}'.format(args, kwargs))
+        return orig_func(*args, **kwargs)
 
-hi_func = outer_function('Hi')
-bye_func = outer_function('Bye')
+    return wrapper
 
-hi_func()
-bye_func()
+def my_timer(orig_func):
+    import time
+
+    @wraps(orig_func)
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        result = orig_func(*args, **kwargs)
+        t2 = time.time() - t1
+        print('{} ran in: {} sec'.format(orig_func.__name__, t2))
+        return result
+
+    return wrapper
+
+import time
+
+@my_logger
+@my_timer
+def display_info(name, age):
+    time.sleep(1)
+    print('display_info ran with arguments ({}, {})'.format(name, age))
+
+
+display_info('Dave', 33)
